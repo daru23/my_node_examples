99db916c945082afa49a99396d4ac111a47eaf60
root <root@localhost.localdomain>
2015-07-31 15:55:29 +0200

    test

--- my_xml/index.js
+++ my_xml/index.js
@@ -7,21 +7,23 @@
 
 var xml2js      = require('xml2js'),
     MongoClient = require('mongodb').MongoClient,
+    JsonDB      = require('node-json-db'),
     readable    = process.stdin ;
 
-/* DB Connection */
+/* DB Connection 
 var saveJSON = function (json, callback){
-   MongoClient.connect('mongodb://localhost:27017/exactTestDB', function(err, db) {
+   MongoClient.connect('mongodb://10.0.1.11:27017/exactTestDB', function(err, db) {
       if(err) console.log(err);
-
       var collection = db.collection('xml2jsonTest');
-      
       collection.insertOne(json, function (err) {
          if (err) callback(err);
          callback('Message was store');
       });
    });
 };
+*/
+
+var db = new JsonDB('exactDB', true, true);
 
 /* Process */
 readable.on('data', function(data, error){
@@ -31,14 +33,18 @@ readable.on('data', function(data, error){
       //Replace $ with 'lambda'
       var jsonString = JSON.stringify(result);
       var cleanjsonString = jsonString.replace(/\$/g, "lambda");
+      
+      db.push('/testObject',JSON.parse(cleanjsonString));
 
-      console.log(cleanjsonString);
-
-      saveJSON(JSON.parse(cleanjsonString), function(message, error){
-         if (error) console.log(error);
-
-         console.log(message);
-      })
-      //console.dir(result);
+      //console.log(cleanjsonString);
+      //saveJSON(JSON.parse(cleanjsonString), function(message, error){
+      //   if (error) console.log(error);
+      //   console.log(message);
+      //   process.exit(0);
+      //})
    });
+});
+
+readable.on('end', function (){
+   console.log('The End!');
 })
